fcb92b620428145ce75f8f44c084d2a7faa7f4db
Krzysztof Tybinka <ktybinka@interia.pl>
2023-09-05 20:04:43 +0200

    fix

--- Analyzer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Analyzer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
