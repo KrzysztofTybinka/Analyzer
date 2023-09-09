8cf4a6708ee86d80d853bc0764505cac1d887912
Krzysztof Tybinka <ktybinka@interia.pl>
2023-09-09 21:37:52 +0200

    Reader class

---
+++ Analyzer/Input.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analyzer
+{
+    public class Input
+    {
+        public Function Function { get; set; }
+        public int Value { get; set; }
+        public string Path { get; set; }
+        public string Node { get; set; }
+    }
+}
