02ab9f4067ed46a1bc9ebf0f391c9b993780f7db
Krzysztof Tybinka <ktybinka@interia.pl>
2023-10-28 15:10:16 +0200

    Employee model added

---
+++ Analyzer/TypedArgument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analyzer
+{
+    public class TypedArgument<T> where T : struct
+    {
+        public T Value { get; init; }
+
+        public TypedArgument(T value)
+        {
+            Value = value;
+        }
+    }
+}
