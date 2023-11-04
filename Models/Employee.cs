a56ae1319f67f5ec563fe903a0a4efe09206059e
Krzysztof Tybinka <ktybinka@interia.pl>
2023-11-04 18:34:18 +0100

    bugs

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Analyzer.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,25 @@ using System.Threading.Tasks;
 
 namespace Analyzer.Models
 {
-    public class Employee 
+    public class Employee : IDateComparable
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public DateTime DateOfBirth { get; set; }
         public double Salary { get; set; }
+
+        public bool Compare(Func<DateTime, bool> operation, string onProperty)
+        {
+            if (onProperty.ToLower() == "dateofbirth")
+            {
+                return operation(DateOfBirth);
+            }
+            if (onProperty.ToLower() == "salary")
+            {
+                return Salary.CompareTo(Salary);
+            }
+            throw new InvalidOperationException();
+        }
     }
 }
