4a7e451bfa494f1ddf543bd0bf0620102fb929f9
Krzysztof Tybinka <ktybinka@interia.pl>
2023-11-05 14:53:34 +0100

    switch command added

---
+++ FileParser/Commands/SwitchCommand.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+using Memory;
+
+namespace FileParser.Commands
+{
+    [Verb("switch", HelpText = "Switches working directory")]
+    internal class SwitchCommand
+    {
+        [Option('n', "name", Required = true, HelpText = "File name")]
+        public string Name { get; set; }
+
+        public void Execute()
+        {
+            string message = ModelCache.Switch(Name);
+            Console.WriteLine(message);
+        }
+    }
+}
