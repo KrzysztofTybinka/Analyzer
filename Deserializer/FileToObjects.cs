beb9642ded605f5df1a8acd919f0fee68e16fdd4
Krzysztof Tybinka <ktybinka@interia.pl>
2023-11-05 12:36:22 +0100

    Summary

--- Deserializer/FileToObjects.cs
+++ Deserializer/FileToObjects.cs
@@ -2,10 +2,20 @@
 
 namespace Analyzer.FileParserAbstractFactory
 {
+    /// <summary>
+    /// Deserializes file into collection of objects.
+    /// </summary>
     public class FileToObjects
     {
         FileToObjects() { }
 
+        /// <summary>
+        /// Reads file and deserializes its content 
+        /// into list of objects.
+        /// </summary>
+        /// <typeparam name="T">Model class</typeparam>
+        /// <param name="path">Path or url to a file</param>
+        /// <returns>List of objects</returns>
         public static List<T> Deserialize<T>(string path) where T : class 
         {
             var reader = ReaderProducer.GetReader(path);
--- Deserializer/ObjectsToFile.cs
+++ Deserializer/ObjectsToFile.cs
@@ -4,10 +4,19 @@ using System.Collections;
 
 namespace Deserializer
 {
+    /// <summary>
+    /// Serializes collection of objects into file.
+    /// </summary>
     public class ObjectsToFile
     {
         ObjectsToFile() { }
 
+        /// <summary>
+        /// Serializes collection of objects into file and saves it
+        /// </summary>
+        /// <param name="path">Location for a file to be saved</param>
+        /// <param name="objects">Collection of objects</param>
+        /// <returns></returns>
         public static string Serialize(string path, IEnumerable objects) 
         {
             try
