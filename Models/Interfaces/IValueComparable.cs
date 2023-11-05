5d0eccc7a12bb75b757f92558065fe4096170b52
Krzysztof Tybinka <ktybinka@interia.pl>
2023-11-05 12:44:07 +0100

    Summary

--- Models/Interfaces/IValueComparable.cs
+++ Models/Interfaces/IValueComparable.cs
@@ -3,6 +3,12 @@ namespace Models.Interfaces
 {
     public interface IValueComparable
     {
+        /// <summary>
+        /// Compares property value declared in string format with given in string format value
+        /// </summary>
+        /// <param name="value">Value to compare</param>
+        /// <param name="propertyName">Property name</param>
+        /// <returns>-1 if value is before property value, 0 if values are equal, 1 if value is after property value</returns>
         public int Compare(string value, string propertyName);
     }
 }
